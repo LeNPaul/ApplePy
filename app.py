aacb60f0df7473604a4066f0d7b17646e8e4c763
Paul Le <l.nguyen.paul@gmail.com>
2019-08-16 11:57:44 -0400

    #1 Update basic Flask application

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/<name>')
 def hello(name=None):
     return render_template('index.html', name=name)
